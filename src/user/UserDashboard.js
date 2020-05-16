b56abf81723cc39e6c8e8d8455c73e26c7bfade3
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-05-16 07:47:44 -0400

    user links

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Layout from '../core/Layout';
-import {isAuthenticated} from '../auth';
+import { isAuthenticated } from '../auth';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
 
-    const {user: {_id, name, email, role}} = isAuthenticated()
+    const { user: { _id, name, email, role } } = isAuthenticated();
 
-    return (
-        <Layout title = "Dashboard"  description="User Dashboard" className="container">
+    const userLinks = () => {
+        return (
+            <div className="card">
+                <h4 className="card-header">User Links</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <Link className="nav-link" to="/cart">My Cart</Link>
+                    </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link" to="/profile/update">Update Profile</Link>
+                    </li>
+
+                </ul>
+            </div>
+        )
+    }
+
+    const userInfo =() =>{
+        return(
             <div className="card mb-5">
             <h3 className="card-header">User Information</h3>
             <ul className="list-group">
@@ -15,14 +33,34 @@ const Dashboard = () => {
                 <li className="list-group-item">{email}</li>
                 <li className="list-group-item">{role === 1 ? "Admin" : "Registered User"}</li>
             </ul>
-            </div>
+        </div>
+        );
+    };
+
+    const purchaseHistory = () =>{
+        return(
             <div className="card mb-5">
             <h3 className="card-header">Purchase History</h3>
             <ul className="list-group">
                 <li className="list-group-item">history</li>
-                
+
             </ul>
-            </div>
+        </div>
+        );
+    };
+
+    return (
+        <Layout title="Dashboard" description={`hello ${name}`} className="container-fluid">
+          <div className="row">
+              <div className="col-3">
+                {userLinks()}
+              </div>
+              <div className="col-9">
+                  {userInfo()}
+                  {purchaseHistory()}
+              </div>
+          </div>
+          
         </Layout>
     );
 };
