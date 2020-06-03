c02dd2a1473e88da9ebaa86c9ca72b68192391c5
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-06-02 22:58:34 -0400

    price range filter option

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -3,11 +3,17 @@ import Layout from './Layout';
 import Card from './Card';
 import { getCategories } from './apiCore';
 import Checkbox from './Checkbox';
+import prices from './fixedPrices';
+import RadioBox from './Radiobox';
 
 const Shop = () => {
 
-    const [categories, setCategories] = useState([])
-    const [error, setError] = useState(false)
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(false);
+    const [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: [] }
+    });
+
 
     //load categories and set form data
     const init = () => {
@@ -25,8 +31,12 @@ const Shop = () => {
         init();
     }, []);
 
-    const handleFilters =(filters, filterBy) => {
-        console.log("SHOP FILTERS", filters, filterBy);
+    const handleFilters = (filters, filterBy) => {
+        // console.log("SHOP FILTERS", filters, filterBy);
+        const newFilters = { ...myFilters };
+        newFilters.filters[filterBy] = filters;
+        setMyFilters(newFilters);
+
     }
 
 
@@ -36,18 +46,28 @@ const Shop = () => {
             <div className="row">
                 <div className="col-4">
                     <h4>Filter by categories</h4>
-                   <ul> 
-                       <Checkbox 
-                         //props
-                        categories = {categories}
-                        handleFilters={ filters => handleFilters(filters, 'category')}
-                        
+                    <ul>
+                        <Checkbox
+                            //props
+                            categories={categories}
+                            handleFilters={filters => handleFilters(filters, 'category')}
+
                         />
-                   </ul>
-        </div>
-                <div className="col-4">
-                    right
-        </div>
+                    </ul>
+
+                    <h4>Filter by Price Range</h4>
+                    <div>
+                        <RadioBox
+                            //props
+                            prices={prices}
+                            handleFilters={filters => handleFilters(filters, 'price')}
+
+                        />
+                    </div>
+                </div>
+                <div className="col-8">
+                    {JSON.stringify(myFilters)}
+                </div>
             </div>
 
 
