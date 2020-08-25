0ca5eb1ea61a47f61fd5e7d296663a864e7bf2b1
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-08-25 09:52:33 -0400

    order status display

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
-import { listOrders } from './apiAdmin';
+import { listOrders, getStatusValues } from './apiAdmin';
 import moment from 'moment';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
+    const [statusValues, setStatusValues] = useState([]);
+
 
     const { user, token } = isAuthenticated();
 
@@ -20,8 +22,21 @@ const Orders = () => {
         });
     };
 
+    const loadStatusValues = () => {
+        getStatusValues(user._id, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setStatusValues(data);
+            }
+        });
+    };
+
+
+
     useEffect(() => {
         loadOrders();
+        loadStatusValues();
     }, []);
 
     const showOrdersLength = () => {
@@ -46,7 +61,29 @@ const Orders = () => {
             readOnly
             />                
         </div>
-    )
+    );
+
+        const handleStatusChange =(e, orderId) =>{
+            console.log('update order status');
+        };
+
+
+    const showStatus = (o) => (
+        <div className='form-group'>
+            <h3 className='mark mb-4'>Status: {o.status}</h3>
+            <select 
+            className='form-control' 
+            onChange={(e)=>handleStatusChange(e,o._id)}
+            >
+                <option>Update Status</option>
+                {statusValues.map((status, index) =>(
+                <option key={index} value={status}>
+                    {status}
+                </option>
+                ))}
+            </select>
+        </div>
+    );
 
     return (
         <Layout
@@ -66,7 +103,7 @@ const Orders = () => {
                                 </h2>
                                 <ul className='list-group mb-2'>
                                     <li className="list-group-item">
-                                        {o.status}
+                                        {showStatus(o)}
                                     </li>
                                     <li className="list-group-item">
                                         Transaction ID: {o.transaction_id}
